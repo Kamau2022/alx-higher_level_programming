48b1115447a8febbfe6707e1dc1d418f4f6df67d
kamau2022 <benkabenson@gmail.com>
2022-09-20 13:15:56 -0700

    first commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+k = int(repr(number)[-1])
+if k == 0:
+    print(f'Last digit of {number} is {k} and is 0')
+elif k < 6 and not 0 and number > 0:
+    print(f'Last digit of {number} is {k} and is less than 6 and not 0')
+elif k > 5 and number > 0:
+    print(f'Last digit of {number} is {k} and is greater than 5')
+elif number < 0:
+    print(f'Last digit of {number} is {k * -1} and is less than 6 and not 0')

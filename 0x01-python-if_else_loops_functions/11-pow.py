7d385d5103e20a42b9f1a48bc0ca13cc58b52048
kamau2022 <benkabenson@gmail.com>
2022-09-21 15:43:04 -0700

    fixed

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -3,13 +3,15 @@
 def pow(a, b):
     i = 1
     power = 1
+    j = 1
     if b >= 0:
         while i <= b:
             power = power * a
             i = i + 1
 
     if b < 0:
-        while i <= -1 * b:
-            power = power * 1 / a
-            i = i + 1
+        while j <= -1 * b:
+            power = power * a
+            j = j + 1
+        power = 1 / power
     return power

213dc28bfd6ef10e2b2631d7fc1939a27ad1d6d3
kamau2022 <benkabenson@gmail.com>
2022-09-24 15:29:26 -0700

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+i = 1;
+if len(sys.argv) - 1 == 1:
+    print(len(sys.argv) - 1, "argument:")
+
+elif len(sys.argv) - 1 == 0:
+    print(len(sys.argv) - 1, "arguments")
+
+elif len(sys.argv) - 1 > 1:
+    print(len(sys.argv) - 1, "arguments:")
+
+while i <= len(sys.argv) - 1:
+    print(f"{i}:", sys.argv[i])
+    i = i + 1

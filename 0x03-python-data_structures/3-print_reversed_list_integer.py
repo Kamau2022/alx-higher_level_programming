c9a3e6894f4b89e091ce629440853dbbaa374a5d
kamau2022 <benkabenson@gmail.com>
2022-09-27 00:13:16 -0700

    added an if statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,13 +3,11 @@ def print_reversed_list_integer(my_list=[]):
     """ this function prints integers in reverse
     Args:
         my_list: a list of integers
-    Returns:
-           nothing
     """
     b = len(my_list)
-    i = -1
-    if not my_list:
-        return 
-    while i >= -b:
-        print("{:d}" .format(my_list[i]))
-        i = i - 1
+    i = -1 
+    if b != 0:
+        while i >= -b:
+            print("{:d}" .format(my_list[i]))
+            i = i - 1
+    return

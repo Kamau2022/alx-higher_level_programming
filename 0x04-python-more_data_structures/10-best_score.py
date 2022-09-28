328528d35a6cce56650bd67f8612e444484a3ab3
kamau2022 <benkabenson@gmail.com>
2022-09-28 15:59:22 -0700

    fixed

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    k = None
+    """ function that finds key with biggest integer value
+    Args:
+      a_dictionary: include keys
+    Returns:
+          a key with the biggest integer value
+    """
+    if a_dictionary == k:
+        return k
+    else:
+        k = max(a_dictionary, key=lambda k: a_dictionary[k])
+        return k

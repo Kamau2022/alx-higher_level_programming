65eee706f11528626161ccce371d90bbdd15231e
kamau2022 <benkabenson@gmail.com>
2022-09-29 00:06:45 -0700

    included None in the module

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,9 +4,12 @@ def print_reversed_list_integer(my_list=[]):
     Args:
         my_list: a list of integers
     """
-    b = len(my_list)
     i = -1
-    if b != 0:
+    k = None
+    if my_list == k:
+        return {}
+    elif my_list != k:
+        b = len(my_list)
         while i >= -b:
             print("{:d}" .format(my_list[i]))
             i = i - 1

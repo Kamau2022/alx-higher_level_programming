f6aec9ab4d12e2fdf96adcf7b832ba0038f157a1
kamau2022 <benkabenson@gmail.com>
2022-09-28 23:06:52 -0700

    included an empty space

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,7 +8,9 @@ def best_score(a_dictionary):
           a key with the biggest integer value
     """
     if a_dictionary == k:
-        return k
+        return None
+    elif a_dictionary == {}:
+        return None
     else:
         k = max(a_dictionary, key=lambda k: a_dictionary[k])
         return k

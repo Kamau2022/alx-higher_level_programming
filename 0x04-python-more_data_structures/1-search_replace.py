74d77575e9a945032e75d854c5080bee691f7e0d
kamau2022 <benkabenson@gmail.com>
2022-09-28 18:29:16 -0700

    first commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """function that replaces all occurrences of an element
+       by another in a new list
+    Args:
+        my_list: is the initial list
+        search: is the element to replace in the list
+        replace: is the new element
+    Returns:
+           new_element
+    """
+    b = len(my_list) - 1
+    new_list = my_list.copy()
+    i = 0
+    for i in range(0, b):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list

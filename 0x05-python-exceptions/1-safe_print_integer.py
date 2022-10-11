fa208b6e501465c9f6ce2aa6a799fee8c2088285
kamau2022 <benkabenson@gmail.com>
2022-10-11 13:54:48 -0700

    first commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """a function that prints an integer
+    Args:
+        value: value to be printed
+    Return: True or False
+    """
+    while True:
+        try:
+            print("{:d}" .format(value), end='')
+        except ValueError:
+            return False
+        else:
+            print()
+            return True

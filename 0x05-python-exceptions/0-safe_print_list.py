004426ded7fc51046f7fb1430a64dac7bb12aeb2
kamau2022 <benkabenson@gmail.com>
2022-10-11 13:01:52 -0700

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    """a function that that prints x elements of a list.
+    Args:
+        my_list: list to print from
+        x: number of elements
+    Returns:
+           real number of elements printed
+    """
+    a = my_list
+    p = 0
+    while p < x:
+        try:
+            print("{}" .format(a[p]), end='')
+            p = p + 1
+        except IndexError:
+            break   
+    print()
+    return p

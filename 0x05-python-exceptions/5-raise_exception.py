a198f8b9672a256224a7e03b668b58bd82f08f55
kamau2022 <benkabenson@gmail.com>
2022-10-12 09:20:06 -0700

    fixed

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def raise_exception():
-   """ a function that raises a type exception.
-   """  
-   while True: 
+    """ a function that raises a type exception.
+    """
+    while True:
         raise TypeError

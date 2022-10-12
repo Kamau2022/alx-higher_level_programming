760f87ee09520e3d2c454ce51284db3e16abecbc
kamau2022 <benkabenson@gmail.com>
2022-10-12 09:39:29 -0700

    first commit

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/phython
+
+def raise_exception_msg(message=""):
+    """a function that raises a name exception with a message
+    Args:
+        message: printed message
+    """
+    raise NameError(message)

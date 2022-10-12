daa04741f5a4ab252c0ca8f57723234eba6a0f4c
kamau2022 <benkabenson@gmail.com>
2022-10-12 01:26:53 -0700

    first commit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """divides 2 integers and prints the result.
+    Args:
+       a: first integer
+       b: second integer
+    Returns: result
+    """
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("inside result: {}".format(result))

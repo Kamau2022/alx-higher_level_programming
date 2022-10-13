1fefe7d6d0086157b409de0d96ba302c227712a9
kamau2022 <benkabenson@gmail.com>
2022-10-13 13:48:09 -0700

    first commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a program that demonstrates use of getter and setter"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, __size=0):
+        self.__size = __size
+
+    def area(self):
+        """a function that returns the current square area"""
+        return(self.__size * self.__size)
+
+    @property
+    def size(self):
+        """a function to retrieve data"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """a function that sets data"""
+        if not(isinstance(value, (int))):
+            raise TypeError("size must be an integer")
+        self.__size = value
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value

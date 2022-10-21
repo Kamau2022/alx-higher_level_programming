1534f09ae47589554a73fec128570af973b0991c
kamau2022 <benkabenson@gmail.com>
2022-10-21 11:29:10 -0700

    pushed

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+import sys
+sys.path.append('/root/alx-higher_level_programming' +
+                '/0x07-python-test_driven_development')
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max(self):
+        self.assertEqual(max_integer([1, 2, 8]), 8)
+        self.assertEqual(max_integer([-1, -2, -8]), -1)
+        self.assertEqual(max_integer([1, 8]), 8)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([]), None)

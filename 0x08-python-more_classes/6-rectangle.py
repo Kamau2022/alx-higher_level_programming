291f44747f0b4ebc1db4c555601b400fc0168329
kamau2022 <benkabenson@gmail.com>
2022-10-23 22:37:16 -0700

    fixed

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """a class that defines a rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
@@ -16,11 +17,12 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if self.__height < 0:
             raise ValueError("height must be >= 0")
-        Rectangle.number_of_instances +=1
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.number_of_instances -=1
+        Rectangle.number_of_instances -= 1
+
     def __str__(self):
         p = int(str(self.__height))
         k = int(str(self.__width))

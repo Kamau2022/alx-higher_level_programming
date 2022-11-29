c2804793a2cba1b57e657e43b81ea2fcf42be2e6
kamau2022 <benkabenson@gmail.com>
2022-11-29 20:05:11 +0000

    comment added

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
--- this script is printing the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- this script is printing all rows of the table first_table from the database hbtn_0c_0 in your MySQL server..
 
 SELECT * FROM first_table 

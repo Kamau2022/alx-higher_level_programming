114cea0d41124044de30f18dd8e2a67997de3095
kamau2022 <benkabenson@gmail.com>
2022-11-30 19:30:29 +0000

    create a table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
 --  a script that creates the table force_name on your MySQL server.
 
-DROP TABLE IF EXISTS force_name; 
 CREATE TABLE IF NOT EXISTS force_name (
 id INT AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL,

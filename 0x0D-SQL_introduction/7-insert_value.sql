51246726c2057cc12c0e1d94b58d85c7ace32b3f
kamau2022 <benkabenson@gmail.com>
2022-12-01 14:55:53 +0000

    insert value

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,4 @@
 -- this script is inserting a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 
-INSERT INTO first_table
-(id, name) auto_increment
-VALUES (89, 'Best School');
+INSERT INTO first_table (id, name)
+VALUES(89, 'Best School');

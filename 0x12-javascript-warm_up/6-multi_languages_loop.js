7218cd4705f728e9a9372211886b9c5ee44b2095
kamau2022 <benkabenson@gmail.com>
2022-12-20 11:55:16 +0000

    an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arraylength = array.length;
+for (let i = 0; i < arraylength; i++) {
+  console.log(array[i]);
+}

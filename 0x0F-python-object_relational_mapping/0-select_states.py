9d10cd03c8b23d27dd78eb5e694370357ed9b711
kamau2022 <benkabenson@gmail.com>
2022-12-24 12:27:50 +0000

    used string formatting

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,16 +3,11 @@
 """
 if __name__ == "__main__":
     import MySQLdb
-    db=MySQLdb.connect(
-    host="localhost",
-    port =3307, 
-    password ="kamau2368", 
-    user="root", 
-    database="hbtn_0e_0_usa"
-    )
+    db = MySQLdb.connect(host="localhost", port=3307, password="kamau2368",
+                         user="root", database="hbtn_0e_0_usa")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
     for row in cursor.fetchall():
-        print (row[0], row[1])
+        print('{}'.format(row))
     cursor.close()
     db.close()

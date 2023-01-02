004ba3e76a73b8c6a106793da24aa7e00b72ac4c
Benson Kamau <benkabenson@gmail.com>
2023-01-02 15:07:51 +0000

    added a join statement

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          user="root", database="hbtn_0e_4_usa")
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name\
-                    FROM cities RIGHT JOIN states\
+                    FROM cities INNER JOIN states\
                     ON cities.state_id = states.id\
                     ORDER by cities.id")
     for row in cursor.fetchall():

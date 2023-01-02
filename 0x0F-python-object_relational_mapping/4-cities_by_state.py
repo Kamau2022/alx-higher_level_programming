386b31d0261c26e0a77feba8fa086297762c059c
Benson Kamau <benkabenson@gmail.com>
2023-01-02 14:58:54 +0000

    added a join statement

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a module that lists all cities from the database hbtn_0e_4_usa
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306, password="kamau2368",
+                         user="root", database="hbtn_0e_4_usa")
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+                    FROM cities RIGHT JOIN states\
+                    ON states.id = cities.state_id\
+                    ORDER by cities.id")
+    for row in cursor.fetchall():
+        print('{}'.format(row))
+    cursor.close()
+    db.close()

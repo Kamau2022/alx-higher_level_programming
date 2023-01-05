affa5392c77aef0c262a74f470b9794f5a440a9f
Benson Kamau <benkabenson@gmail.com>
2023-01-05 19:45:04 +0000

    fixed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""a module that lists all states with a name starting with N
-   from the database hbtn_0e_0_usa
+"""file that contains the class definition of
+   a State and an instance Base = declarative_base()
 """
-if __name__ == "__main__":
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (create_engine)
 import sqlalchemy
-import sys
 
 Base = declarative_base()
 
@@ -18,6 +15,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                           sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)

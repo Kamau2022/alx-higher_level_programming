0d665163c862ad4ba29a7f738e6a5f4af1152843
Benson Kamau <benkabenson@gmail.com>
2023-01-08 12:24:47 +0000

    to be fixed

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,11 +8,13 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           format(sys.argv[1], 'kamau2368', sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     factory = sessionmaker(bind=engine)
     session = factory()
-    for state in session.query(State).filter(State.id == '1'):
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    for state in session.query(State).order_by(State.id):
+        if State.id == 1:
+            print('{}: {}'.format(state.id, state.name))
+        else:
+            print('')

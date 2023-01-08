e8f9520d740f9f5c4122393d886cd1d3de05f31f
Benson Kamau <benkabenson@gmail.com>
2023-01-08 11:38:30 +0000

    added FALSE

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], 'kamau2368', sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    factory = sessionmaker(bind=engine)
+    session = factory()
+    session.query(State).filter(State.name.contains('%a')).delete(False)
+    session.commit()

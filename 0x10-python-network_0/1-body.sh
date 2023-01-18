06b1f9c9076f7fab03ce92aa355a8c0b3b358c8c
Benson Kamau <benkabenson@gmail.com>
2023-01-18 15:26:47 +0000

    fixed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl --write-out "$1" --output output.txt --silent
+curl -sI  --stderr /dev/null "$1" | head -1 | cut -d' ' -f2

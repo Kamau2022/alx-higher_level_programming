e6312658e68622cb44bbccf6f3ccf2447c40d9c7
Benson Kamau <benkabenson@gmail.com>
2023-01-18 14:18:41 +0000

    to be fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-URL="$1"
-curl -sI "$URL"
-grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'

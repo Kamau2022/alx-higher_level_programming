295f30c255da130d56d170d8018c639d4d8fc8d8
kamau2022 <benkabenson@gmail.com>
2023-01-23 09:48:21 -0800

    fixed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,8 @@ import requests
 import sys
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if r.status_code == 200:
-        print("Index")
-    else:
+    if r.status_code >= 400:
         print("Error code: ", end='')
         print(r.status_code)
+    else:
+        print("Index")

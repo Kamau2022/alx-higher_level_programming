f59d1f61f161d3300c40804dc9dfbed40a6a622c
kamau2022 <benkabenson@gmail.com>
2023-04-09 14:38:53 -0700

    sending header with curl request

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to that URL, and displays the body of the response
-curl "$1" -H "X-School-User-Id: 98"
+curl -s "$1" -H "X-School-User-Id: 98"

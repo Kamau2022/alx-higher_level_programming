e64a26ad8874fa9d8e2054fcaf95bd070eed84a7
kamau2022 <benkabenson@gmail.com>
2023-04-09 08:50:07 -0700

    GET method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sI  --stderr /dev/null "$1" | head -1 | cut -d' ' -f2
+# This script takes in a URL, sends a GET request to that URL, and displays the body of the response
+curl -sI "$1" | grep -i HTTP | awk '{print $2}'

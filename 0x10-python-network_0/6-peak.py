71e6af9dd0078fbb557784b820062def2575c6b4
Benson Kamau <benkabenson@gmail.com>
2023-04-10 10:02:52 +0000

    function was documented

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,13 +6,13 @@ def find_peak(list_of_integers):
     """a function to find the peak element"""
     length = len(list_of_integers)
     start = 0
-    end = l - 1
+    end = length - 1
     if length == 0:
         return None
     while start < end:
-        mid = int(start + (end - start) / 2)
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            end = mid
+        middle = int(start + (end - start) / 2)
+        if list_of_integers[middle] > list_of_integers[middle + 1]:
+            end = middle
         else:
-            start = mid + 1
+            start = middle + 1
     return list_of_integers[start]

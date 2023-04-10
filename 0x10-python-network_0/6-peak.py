c393cdba9e9af57ae74c8891ca00ef5f65aeda08
Benson Kamau <benkabenson@gmail.com>
2023-04-10 09:48:43 +0000

    peak element was found

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function to find the peak element"""
+
+
+def find_peak(list_of_integers):
+    length = len(list_of_integers)
+    start = 0
+    end = l - 1
+    if length == 0:
+        return None
+    while start < end:
+        mid = int(start + (end - start) / 2)
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return list_of_integers[start]

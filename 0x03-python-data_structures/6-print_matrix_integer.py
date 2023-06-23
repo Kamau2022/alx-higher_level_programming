fca4c0e87f1ef6c56eaae6ecc1e5a421760e8997
Benson Kamau <benkabenson@gmail.com>
2023-06-23 14:34:45 +0000

    added a space between numbers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """a function to print a formatted matrix"""
-    result = ('\n'.join([''.join(['{:d}'.format(item) for item in row])
+    result = ('\n'.join([' '.join(['{:d}'.format(item) for item in row])
               for row in matrix]))
     print(result)

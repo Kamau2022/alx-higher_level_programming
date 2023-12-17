f45032c177b39925785d13d07e59d4d1975ed8db
Benson Kamau <104911422+Kamau2022@users.noreply.github.com>
2023-12-17 22:02:15 +0300

    Update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,14 @@
 -- this script is creating the database hbtn_0c_0 in your MySQL server.
 -- database is dropped if it  exists
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+
+#!/bin/bash
+
+# MySQL credentials
+DB_USER="your_username"
+DB_PASSWORD="your_password"
+
+# Database name
+DB_NAME="hbtn_0c_0"
+
+# Attempt to create the database
+mysql -u $DB_USER -p$DB_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME" 2>/dev/null

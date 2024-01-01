4995c4c002a9a92668fba7225c9e24d725ccc71e
kamau2022 <benkabenson@gmail.com>
2024-01-01 22:16:32 +0300

    a sub-query was included in the sql statement.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,8 @@
 -- a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-
 SELECT cities.id, cities.name
 FROM cities
-WHERE cities.id = state_id
+WHERE (
+SELECT id
+FROM states 
+WHERE name = 'California') = cities.state_id
 ORDER BY cities.id;
